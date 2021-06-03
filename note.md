f12c668033cd3f377f1cafbdb86eb57538cdc7b0
Alex <alex@q-83.com>
2021-06-03 21:45:54 +1000

    创建了动态路由 {{$route.params.id}} 哪怕是子路由都是可以直接获取饿的

---
+++ note.md
@@ -0,0 +1,9 @@
+### 获取 route param
+```vue  
+<template>
+  <div>
+    a sigle user  {{$route.params.id}}
+  </div>
+</template>
+
+```
---
+++ pages/users/_id/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    a sigle user  {{$route.params.id}}
+  </div>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
